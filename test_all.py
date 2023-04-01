80eba74937f5c83df6c861f83f9f10f67fce33e2
GiggleLiu <cacate0129@gmail.com>
2023-04-01 17:02:41 +0800

    update

--- test_all.py
+++ test_all.py
@@ -1,5 +1,11 @@
-from chatgpt import prompt_manager
+from chatgpt import prompt_manager, help_str, prompt_table
 
 def test_prompt_manager():
-    result = prompt_manager("/polish_en This is a test!")
-    assert isinstance(result, str)
+    for command, prompt in prompt_table.items():
+        result = prompt_manager("%s This is a test!"%command)
+        assert isinstance(result, str)
+    
+    result = prompt_manager("/en-zh This is a test!")
+    assert result == "As a translator, your task is to accurately translate text from English to Chinese. \
+Please pay attention to context and accurately explain phrases and proverbs. \
+Below is the text you need to translate: \n\nThis is a test!"
